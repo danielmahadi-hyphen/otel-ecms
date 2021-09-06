29f320cd808fd94ca9de47b6ddce167b97cdf884
Daniel Mahadi <82790850+danielmahadi-hyphen@users.noreply.github.com>
2021-09-06 19:41:55 +0800

    adds basic otel to proxy

---
+++ proxy/logger.js
@@ -0,0 +1,20 @@
+
+const pino = require("pino");
+const moment = require("moment");
+
+const formatters = {
+  level(label, number) {
+    return { level: label.toUpperCase() };
+  },
+};
+
+const opts = {
+  messageKey: "message",
+  formatters,
+  base: undefined,
+  timestamp: () => `,"timestamp":"${moment(new Date()).format('YYYY-MM-DD HH:mm:ss.SSS')}"`,
+};
+
+const logger = pino(opts);
+
+module.exports.logger = logger;
--- proxy/server.js
+++ proxy/server.js
@@ -4,16 +4,23 @@ const PORT = 3001
 const SERVICE_HOST = process.env.SERVICE_HOST || 'localhost';
 const SERVICE_PORT = process.env.SERVICE_PORT || 3000;
 
-const mockMiddleware = () => {
-  return async (req, res, next) => {
-    console.log('here')
-    req.originalUrl = `https://google.com`
-    next();
-  }
-}
-
 const express = require('express')
 const proxy = require('express-http-proxy')
+const opentelemetry = require('@opentelemetry/api')
+
+const { logger } = require('./logger')
+const tracer = opentelemetry.trace.getTracer('proxy')
+
+const mockMiddleware = () => {
+  return async (req, res, next) => {
+    const mockMiddlewareSpan = tracer.startSpan('static-page-middleware')
+    logger.info('here')
+    req.originalUrl = `https://google.com`
+    mockMiddlewareSpan.end()
+
+    next()
+  }
+}
 
 const app = express()
 
